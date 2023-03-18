013af226590e61285462fce255ec8d47912c2f7f
jagjeet <sappaljagjeet@gmail.com>
2023-03-18 11:05:40 +0530

    updated dropdown

--- client/src/components/Header/dropdown.jsx
+++ client/src/components/Header/dropdown.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './dropdown.css'
+import {useNavigate} from "react-router-dom"
 
 const Dropdown = () => {
+  const navigate=useNavigate();
   return (
     <div className='dropdown-container'>
         <ul className='list'>
             {/* <li className='list-items' style={{fontWeight:"bold"}}>Shop By Category</li>
             <div className='br'></div> */}
-            <li className='list-items'>Headphones</li>
-            <li className='list-items'>Wireless Earbuds</li>
-            <li className='list-items'>Bluetooth Speakers</li>
-            <li className='list-items'>Smart Watches</li>
+            <li className='list-items' onClick={()=>{navigate("/category/1")}}>Headphones</li>
+            <li className='list-items ' onClick={()=>{navigate("/category/4")}}>Wireless Earbuds</li>
+            <li className='list-items' onClick={()=>{navigate("/category/2")}}>Bluetooth Speakers</li>
+            <li className='list-items'onClick={()=>{navigate("/category/3")}}>Smart Watches</li>
         </ul>
     </div>
   )
